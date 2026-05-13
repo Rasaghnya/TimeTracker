212438adaea69fbfcb7a95700f759666ca01c769
Rasaghnya Charuhasa <ramyaarji1@gmail.com>
2026-05-13 19:42:13 +0530

    added dark theme

--- tracktimeapp/urls.py
+++ tracktimeapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
- # path('', views.scan, name='home'),
+    # path('', views.scan, name='home'),
     path('register/', views.register, name='register'),
     path('scan/', views.scan, name='scan'),
     path('success/', views.success, name='success'),
